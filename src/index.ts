2169e47c0fc5f54d3ec7fa3c4092a67eccfd8ff4
Dereje <95926729+Dereje-getu-tadesse-10@users.noreply.github.com>
2024-02-10 22:11:09 +0100

    update: import

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { quotefancy, Quotefancy } from './providers/quotefancy/index'
 import { azquotes, Azquotes } from './providers/azquotes/index'
 
-export { quotefancy, Quotefancy, azquotes, Azquotes }
+export { quotefancy, azquotes, Quotefancy, Azquotes }
